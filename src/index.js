1231a185ec56bbd2e56595776e68191b46e1e038
András Kárász <karaszandris@gmail.com>
2018-05-05 13:49:00 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <p>Hello</p>
+    );
+  }
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
